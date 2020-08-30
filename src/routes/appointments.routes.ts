f031ff4610c313fa4d4b8dd7169bb666e64bad8f
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-08-29 23:44:53 -0300

    Initial Commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsReposistory from '../repositories/AppointmentsReposistory';
+// startOfHour vai pegar a hora passada e colocar minuto e segundos como 0, deixando apenas a hora
+// pasrseISO vai converter um formato String para um formato Date
+
+const appointmentsRouter = Router();
+
+const appointmentsReposistory = new AppointmentsReposistory();
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsReposistory.findByDate(
+    parsedDate
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentsReposistory.create(provider, date);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
