42aaf67b567c2b50f07ff8bd1ce6f30f7cba6fb8
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-09-01 19:55:44 -0300

    Refactoring AppointmentsRepository

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import { startOfHour, parseISO } from 'date-fns';
-import AppointmentsReposistory from '../repositories/AppointmentsReposistory';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 // startOfHour vai pegar a hora passada e colocar minuto e segundos como 0, deixando apenas a hora
 // pasrseISO vai converter um formato String para um formato Date
 
 const appointmentsRouter = Router();
 
-const appointmentsReposistory = new AppointmentsReposistory();
+const appointmentsReposistory = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsReposistory.all();
+
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
