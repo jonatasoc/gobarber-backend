d58474dbe5c5ddc1d7f1494737c1f2c6e18ad227
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-12-17 20:59:26 -0300

    Creating UpdateUserAvatar tests

---
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -0,0 +1,25 @@
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+import UpdateUserAvatarService from './UpdateUserAvatarService';
+
+describe('UpdateUserAvatar', () => {
+  it('should be able to update user avatar', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndow@example.com',
+      password: '123456',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg',
+    });
+
+    expect(user.avatar).toBe('avatar.jpg');
+  });
+});
