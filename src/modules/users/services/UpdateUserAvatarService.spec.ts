9aaf08946ee2ed0a3d280b5e62b131dce3f3cf81
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-12-17 21:11:18 -0300

    Finishing UpdateUserAvatarService test to include exceptions

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -1,6 +1,7 @@
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
 import UpdateUserAvatarService from './UpdateUserAvatarService';
+import AppError from '@shared/errors/AppError';
 
 describe('UpdateUserAvatar', () => {
   it('should be able to update user avatar', async () => {
@@ -21,5 +22,48 @@ describe('UpdateUserAvatar', () => {
     });
 
     expect(user.avatar).toBe('avatar.jpg');
+  });
+
+  it('should not be able to update avatar from non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
+
+    await expect(
+      updateUserAvatar.execute({
+        user_id: 'no-existing-user',
+        avatarFilename: 'avatar.jpg',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should delete old avatar when updating to a new one', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+    const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndow@example.com',
+      password: '123456',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar.jpg',
+    });
+
+    await updateUserAvatar.execute({
+      user_id: user.id,
+      avatarFilename: 'avatar2.jpg',
+    });
+
+    expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
+
+    expect(user.avatar).toBe('avatar2.jpg');
   });
 });
