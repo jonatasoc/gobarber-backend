3fc10a6c8d1ccf4f9e66c8c86f996f211d94951e
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-01-23 21:36:34 -0300

    Add Notifications repository to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -10,11 +10,24 @@ import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepo
 import UserTokensInterface from '@modules/users/repositories/UserTokensInterface';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
+import NotificationRepositoryInterface from '@modules/notifications/repositories/NotificationRepositoryInterface';
+import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
+
 // registerSingleton cria a instância apenas uma vez durante todo o ciclo de vida de nossa aplicação, diferente do método register
 // que cria uma nova instância sempre que é chamado.
 container.registerSingleton<AppointmentsRepositoryInterface>(
   'AppointmentsRepository',
   AppointmentsRepository
 );
-container.registerSingleton<UsersRepositoryInterface>('UsersRepository', UsersRepository);
-container.registerSingleton<UserTokensInterface>('UserTokensRepository', UserTokensRepository);
+container.registerSingleton<UsersRepositoryInterface>(
+  'UsersRepository',
+  UsersRepository
+);
+container.registerSingleton<UserTokensInterface>(
+  'UserTokensRepository',
+  UserTokensRepository
+);
+container.registerSingleton<NotificationRepositoryInterface>(
+  'NotificationsRepository',
+  NotificationsRepository
+);
