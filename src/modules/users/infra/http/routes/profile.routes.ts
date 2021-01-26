87ebb3c315c176367f0fa697729d4b607b6159c7
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-01-26 12:02:57 -0300

    Adding validation to routes

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import ProfileController from '../controllers/ProfileController';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const profileRoute = Router();
 /**
@@ -14,7 +15,19 @@ const profileController = new ProfileController();
 
 profileRoute.use(ensureAuthenticated);
 
-profileRoute.put('/', profileController.update);
+profileRoute.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  profileController.update
+);
 profileRoute.get('/', profileController.show);
 
 export default profileRoute;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import UsersController from '../controllers/UsersController';
 
@@ -18,7 +19,17 @@ const upload = multer(uploadConfig);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      nome: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create
+);
 
 usersRouter.patch(
   '/avatar',
