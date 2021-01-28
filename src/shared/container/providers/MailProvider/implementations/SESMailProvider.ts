68630028d1f745b13d08557115c8bd4c3d332a0a
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-01-28 10:33:44 -0300

    Config SES MailProvider to include AWS region

--- src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -19,6 +19,7 @@ export default class SESMailProvider implements MailProviderInterface {
     this.client = nodemailer.createTransport({
       SES: new aws.SES({
         apiVersion: '2010-12-01',
+        region: 'us-east-2',
       }),
     });
   }
