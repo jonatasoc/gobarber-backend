8c2da05d25f7921d4b502828d27c65701daf8eea
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-01-28 22:12:12 -0300

    Add StorageProvider to index

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,13 +1,3 @@
-import { container } from 'tsyringe';
-
-import StorageProviderInterface from './StorageProvider/models/SotareProviderInterface';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-
+import './StorageProvider';
 import './MailTemplateProvider';
 import './MailProvider';
-// import MailProviderInterface from './MailProvider/models/MailProviderInterface';
-
-container.registerSingleton<StorageProviderInterface>(
-  'StorageProvider',
-  DiskStorageProvider
-);
